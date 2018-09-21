8538eacba686a975e8dd209d1b3ba9ce2bd5be2e
Peter Kamerman <peter.kamerman@gmail.com>
2018-09-21 17:48:53 +0200

    Tried new git config settings

--- git_config.sh
+++ git_config.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/with-contenv bash
 
+# Setup git user information
 GIT_USER=${GIT_USER:=none}
 GIT_EMAIL=${GIT_EMAIL:=none}
 
 if [ "$GIT_USER" != none ]; then
-	echo -e "[user]\n\tname=$GIT_USER\n\temail=$GIT_EMAIL" > /home/rstudio/.gitconfig
+	git config --global user.name $GIT_USER
+	git config --global email $GIT_EMAIL
+	git config --global credential.helper cache
 fi
