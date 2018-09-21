769c1a20a65ebe790663e3a480596dc8a87b3f77
Peter Kamerman <peter.kamerman@gmail.com>
2018-09-21 21:01:06 +0200

    Write config to .gitconfig

--- git_config.sh
+++ git_config.sh
@@ -5,7 +5,6 @@ GIT_USER=${GIT_USER:=none}
 GIT_EMAIL=${GIT_EMAIL:=none}
 
 if [ "$GIT_USER" != none ]; then
-	git config --global user.name "$GIT_USER"
-	git config --global email "$GIT_EMAIL"
+	echo -e "[user]\n\tname=$GIT_USER\n\temail=$GIT_EMAIL" > /home/rstudio/.gitconfig
 	git config --global credential.helper cache
 fi
