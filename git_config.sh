be4aab1479aa07662965ecfb665fe2cce51ef659
Peter Kamerman <peter.kamerman@gmail.com>
2018-09-21 20:57:58 +0200

    Added quotes to git config

--- git_config.sh
+++ git_config.sh
@@ -5,7 +5,7 @@ GIT_USER=${GIT_USER:=none}
 GIT_EMAIL=${GIT_EMAIL:=none}
 
 if [ "$GIT_USER" != none ]; then
-	git config --global user.name $GIT_USER
-	git config --global email $GIT_EMAIL
+	git config --global user.name "$GIT_USER"
+	git config --global email "$GIT_EMAIL"
 	git config --global credential.helper cache
 fi
